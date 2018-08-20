3b3d178f12786cfab77bd5998c82e7db5e12c06e
Aruddy <mxlalruddy@hotmail.com>
2018-08-21 02:40:59 +0800

    使用put_char封装成put_str
    增加函数直接打印字符串，实现方式通过调用put_char

--- kernel/main.c
+++ kernel/main.c
@@ -1,16 +1,6 @@
 #include "print.h"
 int main() {
-	put_char('k');
-	put_char('e');
-	put_char('r');
-	put_char('n');
-	put_char('e');
-	put_char('l');
-	put_char('\n');
-	put_char('1');
-	put_char('2');
-	put_char('\b');
-	put_char('3');
+	put_str("I'm kernel print_str program!\n");
 	while(1);
 	return 0;
 }
